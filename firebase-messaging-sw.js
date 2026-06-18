3aee1d5c3c67523ad01cbf9af662bb5d25f38c5f
adamomic371-star <adamomic371@gmail.com>
2026-06-18 13:04:38 +0200

    Aggiunto firebase-messaging-sw.js per FCM push

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,11 @@
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
+const config={apiKey:"AIzaSyCMPawrAL5tT_bH6YEcNe_UEEyIwLIgHIQ",authDomain:"financeapp-556ae.firebaseapp.com",databaseURL:"https://financeapp-556ae-default-rtdb.europe-west1.firebasedatabase.app",projectId:"financeapp-556ae",storageBucket:"financeapp-556ae.firebasestorage.app",messagingSenderId:"181987533980",appId:"1:181987533980:web:41c5032990ccede17eb959"};
+firebase.initializeApp(config);
+const messaging=firebase.messaging();
+messaging.onBackgroundMessage(payload=>{
+  const n=payload.notification||{};
+  const title=n.title||'Axiom';
+  const options={body:n.body||'',icon:'./app/assets/icons/icon-192.png'};
+  self.registration.showNotification(title,options);
+});

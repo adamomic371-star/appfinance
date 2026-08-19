4d3a2a98ae7251979c4aeb57ed8cb582e6075501
adamomic371-star <adamomic371@gmail.com>
2026-08-19 13:32:52 +0200

    Update transaction_provider.dart

--- axiom_app/lib/src/providers/transaction_provider.dart
+++ axiom_app/lib/src/providers/transaction_provider.dart
@@ -1,19 +1,2 @@
 ﻿import package
-  Map<String, double> getCategoryTotals(int month, int year, String type) {
-+
-    final map = <String, double>{};
-+
-    for (final tx in _transactions) {
-+
-      if (tx.type == type && tx.date.month == month && tx.date.year == year) {
-+
-        map[tx.category] = (map[tx.category] ?? 0) + tx.amount;
-+
-      }
-+
-    }
-+
-    return map;
-+
-  }
-}
+import ../models/transaction.dart
